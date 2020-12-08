ee04d4a70589d336ff1faa12622987b2c3fd3186
Emanuel <emanuel@aijunasys.com.ar>
2020-12-08 00:51:51 -0300

    Day 06.

---
+++ AOC2020.playground/Sources/day06.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func groupAnswers(everyone: Bool) -> [Set<Character>] {
+    let lines = readLines("day06")
+    let all = Set("abcdefghijklmnopqrstuvwxyz")
+    let none = Set<Character>()
+    var answers = [Set<Character>]()
+    var current = everyone ? all : none
+    var touched = false
+    for line in lines {
+        if line == "" {
+            answers.append(current)
+            current = everyone ? all : none
+            touched = false
+        }
+        else {
+            current = everyone ? current.intersection(line) : current.union(line)
+            touched = true
+        }
+    }
+    if touched {
+        answers.append(current)
+    }
+    return answers
+}
+
+public func day06a() -> Int {
+    return groupAnswers(everyone: false).lazy
+        .reduce(0) { $0 + $1.count }
+}
+
+public func day06b() -> Int {
+    return groupAnswers(everyone: true).lazy
+        .reduce(0) { $0 + $1.count }
+}
