5db824be4dbda74054a4afa0319dcbf8feeca473
Emanuel <emanuel@aijunasys.com.ar>
2020-12-07 23:46:07 -0300

    Day 05.

---
+++ AOC2020.playground/Sources/day05.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func decodeId(string: Substring) -> Int {
+    string.reduce(0) { $0 * 2 + ($1 == "B" || $1 == "R" ? 1 : 0) }
+}
+
+private func parseLines() -> [Int] {
+    readLines("day05").lazy.filter { $0 != "" }
+        .map(decodeId)
+}
+
+public func day05a() -> Int {
+    parseLines().max()!
+}
+
+public func day05b() -> Int {
+    let ids = parseLines()
+    let first = ids.first!
+    return parseLines().reduce((min: first, max: first, missing: Set<Int>())) { (prev, id) in
+        if id < prev.min {
+            return (min: id, max: prev.max, missing: prev.missing.union((id+1)..<prev.min))
+        }
+        if id > prev.max {
+            return (min: prev.min, max: id, missing: prev.missing.union((prev.max+1)..<id))
+        }
+        return (min: prev.min, max: prev.max, missing: prev.missing.subtracting([id]))
+    }
+    .missing.first!
+}
